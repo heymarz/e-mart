4dda60e7ee59eb16d76e05059938093988f960e2
Marlena Tsang <Marlena345@gmail.com>
2022-06-03 14:34:06 -0400

    changed from buyer_id to user_id

--- db/migrate/20220601213116_create_reviews.rb
+++ db/migrate/20220601213116_create_reviews.rb
@@ -2,7 +2,7 @@ class CreateReviews < ActiveRecord::Migration[7.0]
   def change
     create_table :reviews do |t|
       t.text :userReview
-      t.integer :buyer_id
+      t.integer :user_id
 
       t.timestamps
     end
