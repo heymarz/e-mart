ba53e31a111c861207bc4ec22518a78f4ac34ba4
Marlena Tsang <Marlena345@gmail.com>
2022-06-03 14:34:35 -0400

    added login, signup, logout routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  get '/me', to: 'user#show'
+
+  post '/signup', to: 'users#create'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#delete'
+
+
   resources :for_sale_items
 end
