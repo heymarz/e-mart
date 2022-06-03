f51b5c03ec90d4d4b23fb4b3b08a6078579cbd0e
Marlena Tsang <Marlena345@gmail.com>
2022-06-03 14:33:45 -0400

    forSaleItems table

---
+++ db/migrate/20220602123816_create_for_sale_items.rb
@@ -0,0 +1,14 @@
+class CreateForSaleItems < ActiveRecord::Migration[7.0]
+  def change
+    create_table :for_sale_items do |t|
+      t.integer :user_id
+      t.integer :category_id
+      t.string :itemTitle
+      t.integer :itemPrice
+      t.string :itemImg
+      t.string :itemDescription
+
+      t.timestamps
+    end
+  end
+end
