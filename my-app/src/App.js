bdd337af2a0abfda6bc50ff94c6a4e0d159164c7
Marlena Tsang <Marlena345@gmail.com>
2022-06-03 17:50:17 -0400

    wrap routes for home & login components in router

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,20 @@
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import LoginForm from "./components/static/LoginForm"
+import Home from "./components/pages/Home"
 
 function App() {
   return (
     <Router>
       <main>
         <Routes>
-          
+          <Route
+            path="/"
+            element={<Home />}
+          />
+          <Route
+            path="login"
+            element={<LoginForm />}
+          />
         </Routes>
       </main>
     </Router>
