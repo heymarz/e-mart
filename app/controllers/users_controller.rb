43c551a76da2580dba4db154f59863501f226224
Marlena Tsang <Marlena345@gmail.com>
2022-06-09 15:54:43 -0400

    changed name of users_controller

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,28 @@
+class UsersController < ApplicationController
+  skip_before_action :authorize, only: [:create]
+
+  def create
+    user = User.create(user_params)
+      session[:user_id] = user.id
+      if user.valid?
+        render json: user, status: :created
+      else
+        binding.pry
+        render json: { error: user.errors.full_messages.join(", ") }, status: :unprocessable_entity
+      end
+  end
+  
+  def show
+    user = User.find_by(id: session[:user_id])
+    if user
+      render json: user
+    else
+      render json: {error: "Not authorized"}, status: :unauthorize
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:username, :email, :password, :password_confirmation)
+  end
+end
