fe36f434ee581de2b6722779fcd959db0344734d
Marlena Tsang <Marlena345@gmail.com>
2022-06-09 22:09:37 -0400

    corrected /logout route

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,7 @@ Rails.application.routes.draw do
 
   post '/signup', to: 'users#create'
   post '/login', to: 'sessions#create'
-  delete '/logout', to: 'sessions#delete'
-
+  delete '/logout', to: 'sessions#destroy'
 
   resources :for_sale_items
 end
