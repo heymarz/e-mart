7076819f3f23497b55ee3e1bbd38bbb0d55398c2
Marlena Tsang <Marlena345@gmail.com>
2022-06-09 22:12:25 -0400

    updated authorize method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,15 +1,10 @@
 class ApplicationController < ActionController::API
   include ActionController::Cookies
-  
-  rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
-
   before_action :authorize
 
   private
   def authorize
-    @current_user = User.find_by(id: session[:user_id])
-
-    render json: { errors: ["Not Authorized"] }, status: :unauthorized unless @current_user
+    return render json: { errors: ["Not Authorized"] }, status: :unauthorized unless session.include? :user_id
   end
 
   def render_unprocessable_entity_response(exception)
