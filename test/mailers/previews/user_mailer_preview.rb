16e8af572cfc1b6fd9d8517e6c86072409a7ff48
Marlena Tsang <Marlena345@gmail.com>
2022-06-30 12:34:12 -0400

    user_mailer intital

---
+++ test/mailers/previews/user_mailer_preview.rb
@@ -0,0 +1,10 @@
+# Preview all emails at http://localhost:3000/rails/mailers/user_mailer
+class UserMailerPreview < ActionMailer::Preview
+  
+  def welcome_email(user, message)
+    @user = User.last
+    @message= "hi"
+    UserMailer.welcome_email(user) 
+  end
+
+end
