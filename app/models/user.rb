bf69c8aefc33bf2bc0489c47f6267e2790dca7f0
Marlena Tsang <Marlena345@gmail.com>
2022-06-30 12:36:27 -0400

    User model with validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,15 @@ class User < ApplicationRecord
   validates_length_of :username, in: 3..10
   validates :email, uniqueness: { case_sensitive: false }
   validates_presence_of :username, :email
-  validates_format_of :email, with: URI::MailTo::EMAIL_REGEXP
+  validate :valid_email?
 
   has_many :forSaleItems
   has_many :categories, through: :forSaleItems
+
+
+  def valid_email? 
+      errors.add(:email, "invalid") unless
+      self.email.match(/[\w\d._%+-]+@[\w\d.-]+\.[\w]{2,4}/)
+  end
+  
 end
