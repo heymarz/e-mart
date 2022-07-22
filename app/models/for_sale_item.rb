282800f85c74390e4ae7673d1842068dfa6b2205
Marlena Tsang <Marlena345@gmail.com>
2022-07-22 12:12:04 -0400

    image type method refractored

--- app/models/for_sale_item.rb
+++ app/models/for_sale_item.rb
@@ -4,20 +4,28 @@ class ForSaleItem < ApplicationRecord
   validates :itemTitle, :itemDescription, presence: true
   validate :image_type
   validates :itemPrice, numericality: { greater_than: 0, less_than: 10000 }
-  has_many_attached :images do |attachable|
-    attachable.variant :thumb, resize_to_limit: [100, 100]
-  end
+
+  has_many_attached :images
+  
+  # .each do |attachable|
+  #   attachable.representation(resize_to_limit: [100, 100]).processed.url
+  # end
+  
+  # with_all_variant_records.each do |image|
+      # image_tag image.representation(resize_to_limit: [100, 100]).processed.url
+    # end
+
 
   private
   def image_type
     if images.attached? == false
       errors.add(:images, "are missing!")
-    end
     images.each do |image|
-      if !image.content_type.in?(%('image/jpeg image/png'))
-        errors.add(:images, 'needs to be a JPEG or PNG')
-      end
+        if !image.content_type.in?(%('image/jpeg image/png'))
+          errors.add(:images, 'needs to be a JPEG or PNG')
+        end
     end
   end
+end
 
 end
