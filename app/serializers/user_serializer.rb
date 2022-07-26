f117641e27eb553801466110c8b3584574e63304
Marlena Tsang <Marlena345@gmail.com>
2022-07-26 15:22:56 -0400

    added valid_email method in User Serializer

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,3 +1,13 @@
 class UserSerializer < ActiveModel::Serializer
   attributes :id, :username, :password_digest
+
+  has_many :forSaleItems
+  has_many :categories, through: :forSaleItems
+
+
+  def valid_email? 
+      errors.add(:email, "invalid") unless
+      self.email.match(/[\w\d._%+-]+@[\w\d.-]+\.[\w]{2,4}/)
+  end
+  
 end
