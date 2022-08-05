63bcbce21d1046be1215db0d3c475d17110cc4e7
Marlena Tsang <Marlena345@gmail.com>
2022-08-05 17:11:06 -0400

    refractored to show button to Details pg

--- my-app/src/components/pages/Posts/PostCards.jsx
+++ my-app/src/components/pages/Posts/PostCards.jsx
@@ -1,28 +1,30 @@
 import React from 'react'
-import { BsFillTrashFill } from "react-icons/bs";
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
-import './PostCards.scss'
+import './PostCards.scss';
+// import Form from "react-bootstrap/Form";
 
-function PostCards({ item, handleDeleteItem }){
-  const { id, itemTitle, itemDescription, itemPrice, user_id } = item
+function PostCards({ item, showDetails }){
+  // const [favorite, setFavorite] = useState("")
+  const { id, itemTitle, itemDescription, itemPrice } = item
 
-  function handleDeleteClick(e){
-    e.preventDefault()
-    fetch(`/for_sale_items/${id}`,{
-      method: "DELETE"
-    });
-    handleDeleteItem(id)
-  }
+  // function handleFavorite(e){
+  //   e.preventDefault();
+  //   setFavorite()
+  //   console.log(e)
+  // }
+  
+
   return (
     <div className='cards'>
       <Card style={{ width: '18rem' }}>
-        <Card.Img variant="top" src="holder.js/100px180" />
+        <Card.Img variant="top" src="http://placeimg.com/300/300/animals" />
         <Card.Body>
           <Card.Title>{itemTitle}</Card.Title>
           <Card.Text>{itemPrice}</Card.Text>
           <Card.Text>{itemDescription}</Card.Text>
-          <Button variant="outline-info" size="sm" onClick={handleDeleteClick}><BsFillTrashFill /></Button>
+          {/* <Button onClick={handleFavorite}>Favorite</Button> */}
+          <Button onClick={()=>showDetails(id)}>More Info</Button>
         </Card.Body>
       </Card>
     </div>
