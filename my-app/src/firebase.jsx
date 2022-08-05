11ea3c1534ddc177689ddceebee1798d50a6b9e9
Marlena Tsang <Marlena345@gmail.com>
2022-08-05 17:03:58 -0400

    firebase code start up

---
+++ my-app/src/firebase.jsx
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getStorage } from 'firebase/storage';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAaxWZO4o8TdPpF-r-xrTzGSDOkFlvvkug",
+  authDomain: "e-mart-imgs.firebaseapp.com",
+  projectId: "e-mart-imgs",
+  storageBucket: "e-mart-imgs.appspot.com",
+  messagingSenderId: "897038941837",
+  appId: "1:897038941837:web:b34ccecc113e01f46d2cc4",
+  measurementId: "G-HL8H6D8HVQ"
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+export const storage = getStorage(app);
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,17 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { Provider } from "react-redux";
 import App from "./App";
-
-// import { composeWithDevTools } from "redux-devtools-extension";
-// const composedEnhancer = composeWithDevTools(applyMiddleware(thunkMiddleware));
-import { store } from "./store"
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
       <App />
-    </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
