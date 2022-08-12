4dccb27b038b6c4afc956d87db284ae536def8bb
Marlena Tsang <Marlena345@gmail.com>
2022-08-11 21:26:44 -0400

    passing props down to container

--- my-app/src/components/pages/Home.jsx
+++ my-app/src/components/pages/Home.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import PostsContainer from "./Posts/PostsContainter";
 
-function Home () {
+function Home ({saleItems, handleSearch, search}) {
   return (
     <div>
-      <PostsContainer />
+      <PostsContainer saleItems={saleItems} handleSearch={handleSearch} search={search}/>
     </div>
   )
 }
