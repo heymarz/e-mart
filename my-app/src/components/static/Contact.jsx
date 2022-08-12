a96d81a82bc6d40e5ca2dff236845c4c4a4a6707
Marlena Tsang <Marlena345@gmail.com>
2022-08-11 21:30:09 -0400

    Contacts page

---
+++ my-app/src/components/static/Contact.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Contact() {
+  return (
+    <div>
+      <h1 className='header'>Contact me:</h1>
+
+      <h3 className='center'><a href="https://www.linkedin.com/in/marlena345/">Marlena's LinkedIn</a></h3>
+
+      <h3 className='center'><a href='https://github.com/heymarz'>Marlena's Github</a></h3>
+
+    </div>
+  )
+}
+
+export default Contact
