b5034957fda1339323470af19e8ea9d61140a3e8
Marlena Tsang <Marlena345@gmail.com>
2022-08-14 21:09:07 -0400

    edit toggle and user toggle

--- my-app/src/components/pages/Posts/PostDetails.jsx
+++ my-app/src/components/pages/Posts/PostDetails.jsx
@@ -1,26 +1,25 @@
 import React, {useEffect, useState} from 'react'
 import { BsFillTrashFill } from "react-icons/bs";
 import { useParams, useNavigate } from 'react-router-dom';
-// import EditPost from './EditPost';
+import EditPost from './EditPost';
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
 
-function PostDetails({user, handleDelete}){
+function PostDetails({user, onUpdatePost}){
+  let {forSaleItemId} = useParams();
   const [currentForSaleItems, setCurrentForSaleItems] = useState([]);
-  // const [isEditing, setIsEditing] = useState(false);
-  const { postTitle, itemPrice, itemDescription, category_id } = currentForSaleItems
-  let {post_id} = useParams();
+  const { itemTitle, itemPrice, itemDescription, images, category_id } = currentForSaleItems
+  const [isEditing, setIsEditing] = useState(false);
   const navigate = useNavigate();
-  
+
   useEffect(()=>{
-    fetch(`/for_sale_items/${post_id}`)
+    fetch(`/for_sale_items/${forSaleItemId}`)
     .then((r)=>r.json())
     .then((data)=>setCurrentForSaleItems(data))
   },[])
 
-// console.log(currentForSaleItems)
-  function handleDelete(post_id){
-    fetch(`/for_sale_items/${post_id}`,{
+  function handleDelete(forSaleItemId){
+    fetch(`/for_sale_items/${forSaleItemId}`,{
       method: "DELETE",
     }).then(()=>{
       handleDelete(currentForSaleItems.id)
@@ -29,22 +28,24 @@ function PostDetails({user, handleDelete}){
   }
 
   return (
-    <div>
-      <h1>{postTitle}</h1>
-      <Card.Img src="http://placeimg/com/300/300/animals" />
+    <div className='text-container'>
+      {isEditing ? (<EditPost editPost={isEditing} setIsEditing={setIsEditing} onUpdatePost={onUpdatePost} currentForSaleItems={currentForSaleItems}/>) : (<div>
+      <h1>{itemTitle}</h1>
+      <Card.Img src={images} />
       <Card.Text>$${itemPrice}</Card.Text>
       <Card.Text>{category_id}<br/>{itemDescription}</Card.Text>
-      {user ? <div>
-        <Button>Edit</Button>
-        <Button variant="outline-info" size="sm" onClick={handleDelete(post_id)}>
+      {user === currentForSaleItems.user ? (<div>
+        <Button onClick={()=>setIsEditing(currentForSaleItems)}>Edit</Button>
+        <Button variant="outline-info" size="sm" onClick={()=>{handleDelete(forSaleItemId)}}>
             <BsFillTrashFill />
-        </Button>
-        </div>
+        </Button></div>)
         :
-        <Button>Click here to contact Seller</Button> 
-        }
+        (<a href={`mailto:${currentForSaleItems.user.email}?subject=${currentForSaleItems.itemTitle}`}>Click here to contact Seller</a> )
+      }
+        </div>
+      )}
+        </div>
       
-    </div>
   )
 }
 
