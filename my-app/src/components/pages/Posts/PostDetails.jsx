20f80fed24f1d60684492ee33867d80a50d67dd2
Marlena Tsang <Marlena345@gmail.com>
2022-08-24 15:14:47 -0400

    add imgs fx, fix id error, refresh ok

--- my-app/src/components/pages/Posts/PostDetails.jsx
+++ my-app/src/components/pages/Posts/PostDetails.jsx
@@ -1,60 +1,48 @@
-import React, {useEffect, useState, useContext} from 'react';
-import DataContext from '../../../DataContext';
+import React, {useEffect, useState} from 'react';
 import { BsFillTrashFill } from "react-icons/bs";
-import { useParams, useNavigate } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 import EditPost from './EditPost';
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
-import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { Carousel } from 'react-responsive-carousel';
 
 function PostDetails(){
-  let {forSaleItemId} = useParams();
-  const [currentForSaleItems, setCurrentForSaleItems] = useState({user: {}});
-  const { itemTitle, itemPrice, itemDescription, user } = currentForSaleItems
+  const {forSaleItemId} = useParams();
+  const [currentForSaleItems, setCurrentForSaleItems] = useState({user: {}, images: ""});
+  const { itemTitle, itemPrice, itemDescription, user_id, user } = currentForSaleItems
   const [isEditing, setIsEditing] = useState(false);
-  const {currentUser} = useContext(DataContext)
   const navigate = useNavigate();
 
   useEffect(()=>{
     fetch(`/for_sale_items/${forSaleItemId}`)
     .then((r)=>r.json())
-    .then((data)=> setCurrentForSaleItems(data))
+    .then((data)=> {
+      setCurrentForSaleItems(data)})
   },[])
+
+  const imgs = currentForSaleItems.images.split(", ");
   
-  function handleDelete(forSaleItemId){
+  function handleDelete(){
     fetch(`/for_sale_items/${forSaleItemId}`,{
       method: "DELETE",
     }).then(()=>{
-      handleDelete(currentForSaleItems.id)
+      handleDelete(forSaleItemId)
       navigate("/")
     })
   }
-
-  function renderImgs(){
-    if(currentForSaleItems.images){
-      const imgs = currentForSaleItems.images.split(", ");
-      const displayImgs = imgs.map((img, index)=>{
-    return (<Card.Img className="images" src={img} key={index} alt={img}/>)
-  });
-    return displayImgs
-    }else{
-      return (<p>image Error</p>)
-    }
-  }
-
+  
   function renderContact(){
-    if(currentUser === user){
+    if(user.id === user_id){
       return (<div>
         <Button onClick={()=>setIsEditing(currentForSaleItems)}>Edit</Button>
-        <Button variant="outline-info" size="sm" onClick={()=>{handleDelete(forSaleItemId)}}>
+        <Button variant="outline-info" size="sm" onClick={()=>{handleDelete(forSaleItemId)}}> 
             <BsFillTrashFill />
-        </Button>
+        </Button> 
       </div>
       )
     }
     else{
-
       return (<div><a href={`mailto:${currentForSaleItems.user.email}?subject=${currentForSaleItems.itemTitle}`}>Click here to contact Seller</a></div>)
     }
   }
@@ -63,12 +51,13 @@ function PostDetails(){
     <div className='text-container'>
       {isEditing ? (<EditPost editPost={isEditing} setIsEditing={setIsEditing} currentForSaleItems={currentForSaleItems}/>) : (<div>
           <h1>{itemTitle}</h1>
-          <Carousel>
-            {renderImgs()}
+          <Carousel infiniteLoop showThumbs>
+          {imgs.map((img, index)=>{
+        return (<img className="images" src={img} key={index} alt={img}/>)})}
           </Carousel>
           <Card.Text>Asking Price:<span className='green'> $ </span>{itemPrice}</Card.Text>
           <Card.Text><span className='scriptHeader'>Owner Comments: </span><span className='commentBox'>{itemDescription}</span></Card.Text>
-          {renderContact()}
+          {renderContact()}    
         </div>
         )}
       </div>
