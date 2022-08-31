11964faef0eefced0a1d1d1d72727cd6afe428bd
Marlena Tsang <Marlena345@gmail.com>
2022-08-31 15:54:35 -0400

    added Favorites relationship

--- app/models/for_sale_item.rb
+++ app/models/for_sale_item.rb
@@ -1,6 +1,8 @@
 class ForSaleItem < ApplicationRecord
   belongs_to :user
   belongs_to :category
+  has_many :favorites
+
   validates :images, presence: true
   validates :itemTitle, :itemDescription, presence: true
   validates :itemPrice, numericality: { greater_than: 0, less_than: 10000 }
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,12 +2,13 @@ class User < ApplicationRecord
   has_secure_password
 
   validates_length_of :username, in: 3..10
-  validates :email, uniqueness: true, on: :account_setup
+  # validates :email, uniqueness: true, on: :account_setup
   validates_presence_of :username, :email
   validate :valid_email?
 
-  has_many :forSaleItems
- 
+  has_many :for_sale_items
+  has_many :favorites
+
   def valid_email? 
       errors.add(:email, "invalid") unless
       self.email.match(/[\w\d._%+-]+@[\w\d.-]+\.[\w]{2,4}/)
