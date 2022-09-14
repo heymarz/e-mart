3ec454da2037705da7ef4980256bdf454973c461
Marlena Tsang <Marlena345@gmail.com>
2022-09-13 22:59:07 -0400

    delete fav with saleItem

--- app/models/for_sale_item.rb
+++ app/models/for_sale_item.rb
@@ -1,7 +1,7 @@
 class ForSaleItem < ApplicationRecord
   belongs_to :seller, class_name: "User"
 
-  has_many :favorites
+  has_many :favorites, dependent: :delete_all
   has_many :buyers, through: :favorites
   belongs_to :category
 
