57256af537c3afaab4eecb441c4846515ba2e70b
Marlena Tsang <Marlena345@gmail.com>
2022-09-13 22:28:16 -0400

    restful favorite routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,13 +4,15 @@ Rails.application.routes.draw do
   post '/signup', to: 'users#create'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
-
+  
   get '/categories', to: 'categories#index'
   get '/images', to: 'images#index'
-
-  root "welcome#index"
+  
   resources :for_sale_items, except: [:new]
-
-  post '/favorites/for_sale_item/:forSaleItemId', to: 'favorites#create'
-  delete '/favorites/for_sale_item/:forSaleItemId', to: 'favorites#destroy'
+  
+  get '/favorites', to: 'favorites#index'
+  post '/favorites', to: 'favorites#create'
+  delete '/favorites/:id', to: 'favorites#destroy'
+  
+  root "welcome#index"
 end
