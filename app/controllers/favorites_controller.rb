ce870b750f9c6b39be1c065a0590a6815d6cec0b
Marlena Tsang <Marlena345@gmail.com>
2022-09-13 22:29:10 -0400

    refractored created, renamed show to index

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,26 +1,19 @@
-require 'pry'
 class FavoritesController < ApplicationController
+  def index
+    user = User.find_by(id: session[:user_id])
+    render json: user.favorites
+  end
+  
   def create
     @favorite = Favorite.create(favorite_params)
-    buyer = Favorite.find_by_buyer_id(params[:buyer_id])
-    if @favorite.buyer_id === buyer
-      binding.pry
-      redirect_to action: :destroy
-    else
-      @favorite.save
-      render json: @favorite, status: :created
+    render json: @favorite, status: :created
   end
-end
+
 
   def destroy
     @favorite = Favorite.find_by(id: params[:id])
     @favorite.destroy
     head :no_content
-  end
-
-  def show
-    user = User.find_by(id: session[:user_id])
-    render json: user.wishListItems
   end
 
   private
