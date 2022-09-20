7eba5db7050e3a3705052da9d43eaad44e582da2
Marlena Tsang <Marlena345@gmail.com>
2022-09-20 00:51:42 -0400

    disabled annoying clearError Error

--- my-app/src/components/static/LoginForm.jsx
+++ my-app/src/components/static/LoginForm.jsx
@@ -37,6 +37,7 @@ function LoginForm() {
 
     useEffect(()=>{
       return()=>clearErrors();
+      //eslint-disable-next-line react-hooks/exhaustive-deps
     },[])
       
   return (
--- my-app/src/components/static/SignupForm.jsx
+++ my-app/src/components/static/SignupForm.jsx
@@ -42,6 +42,7 @@ function SignupForm() {
 
   useEffect(()=>{
     return()=>clearErrors()
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   },[])
 
   return (
