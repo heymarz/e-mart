d20ba69796f18fe6b8d93846f90c1ca799eca96b
Marlena Tsang <Marlena345@gmail.com>
2022-09-30 18:04:17 -0400

    changed integer to t.references

--- db/migrate/20220829010827_create_favorites.rb
+++ db/migrate/20220829010827_create_favorites.rb
@@ -1,9 +1,8 @@
 class CreateFavorites < ActiveRecord::Migration[7.0]
   def change
     create_table :favorites do |t|
-      t.references :buyer
-      t.integer "for_sale_item_id"
+      t.references :buyer, foreign_key: {to_table: :users}
+      t.references :item, foreign_key: true
     end
-    add_foreign_key "favorites", "users", column: "buyer_id"
   end
 end
